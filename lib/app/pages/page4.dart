95fe32d8993ea4ffe98fba74c74a4cd5a2214299
DevDouglasConectado <douglasrodriguesconectadoifmg@gmail.com>
2021-04-12 06:57:56 -0300

    commit pagina04

---
+++ lib/app/pages/page4.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:navegacao_douglas/app/pages/page1.dart';
+import 'package:navegacao_douglas/app/pages/page1.dart';
+//import 'package:navigator2/app/pages/page1.dart';
+
+class Page3 extends StatelessWidget {
+  const Page3({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 4'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Page 4'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Page1(),
+                  ),
+                );
+              },
+              child: Text('Vai para página 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.popUntil(context, (route) => route.isFirst);
+
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Page1()),
+                );
+              },
+              child: Text('Vai para página 1 e fecha todas anteriores'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
